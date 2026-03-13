1d52391efdbd78e2890f204b1980ba2bcaa4b75e
ehulland <e.frame@erasmusmc.nl>
2026-03-13 16:05:20 +0100

    Replace social text links with icons; add Google Scholar link

--- scripts/build_site_pages.py
+++ scripts/build_site_pages.py
@@ -132,6 +132,11 @@ def page_shell(title: str, content: str, site_title: str):
 '''
 
 
+ICON_GITHUB = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" aria-hidden="true"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>'
+ICON_LINKEDIN = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" aria-hidden="true"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>'
+ICON_X = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" aria-hidden="true"><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>'
+ICON_SCHOLAR = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" aria-hidden="true"><path d="M12 3L1 9l11 6 11-6-11-6zM5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82z"/></svg>'
+
 def render_home(site_title, author, avatar):
     name = author.get("name", "")
     bio = author.get("bio", "")
@@ -139,14 +144,17 @@ def render_home(site_title, author, avatar):
     github = author.get("github", "")
     linkedin = author.get("linkedin", "")
     twitter = author.get("twitter", "")
+    googlescholar = author.get("googlescholar", "")
     social = []
     if github:
-        social.append(f'<a href="https://github.com/{html.escape(github)}">GitHub</a>')
+        social.append(f'<a class="social-icon" href="https://github.com/{html.escape(github)}" aria-label="GitHub" title="GitHub">{ICON_GITHUB}</a>')
     if linkedin:
-        social.append(f'<a href="https://www.linkedin.com/in/{html.escape(linkedin)}">LinkedIn</a>')
+        social.append(f'<a class="social-icon" href="https://www.linkedin.com/in/{html.escape(linkedin)}" aria-label="LinkedIn" title="LinkedIn">{ICON_LINKEDIN}</a>')
     if twitter:
-        social.append(f'<a href="https://x.com/{html.escape(twitter)}">X</a>')
-    social_html = " &middot; ".join(social)
+        social.append(f'<a class="social-icon" href="https://x.com/{html.escape(twitter)}" aria-label="X (Twitter)" title="X (Twitter)">{ICON_X}</a>')
+    if googlescholar:
+        social.append(f'<a class="social-icon" href="{html.escape(googlescholar)}" aria-label="Google Scholar" title="Google Scholar">{ICON_SCHOLAR}</a>')
+    social_html = '<div class="social-icons">' + "".join(social) + '</div>' if social else ""
 
     content = f'''
   <section class="hero hero-profile">
@@ -155,7 +163,7 @@ def render_home(site_title, author, avatar):
         <h1>{html.escape(name)}</h1>
         <p class="lead">{html.escape(bio)}</p>
         <p><strong>Location:</strong> {html.escape(location)}</p>
-        <p>{social_html}</p>
+        {social_html}
         <p><a class="button" href="/about.html">Learn more</a></p>
       </div>
       <div class="profile-photo-wrap">
